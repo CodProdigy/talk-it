37d483ea94bc72d570af62f4552d103df204a889
CodProdigy <165462522+CodProdigy@users.noreply.github.com>
2025-04-05 23:55:54 +0530

    Creating and sending a new message with image upload handling.

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -33,30 +33,30 @@ export const getMessages = async (req, res) => {
     }
 };
 
-// export const sendMessages = async (req, res) => {
-//     try {
-//         const { text, image } = req.body;
-//         const { id:receiverId } = req.params;
-//         const senderId = req.user._id;
+export const sendMessages = async (req, res) => {
+    try {
+        const { text, image } = req.body;
+        const { id:receiverId } = req.params;
+        const senderId = req.user._id;
 
-//         let imageUrl;
-//         if (image) {
-//             const uploadResponse = await cloudinary.uploader.upload(image);
-//             imageUrl = uploadResponse.secure_url;
-//         }
+        let imageUrl;
+        if (image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
 
-//         const newMessage = new Message({
-//             senderId,
-//             receiverId,
-//             text,
-//             image:imageUrl,
-//         });
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image:imageUrl,
+        });
 
-//         await newMessage.save();
+        await newMessage.save();
 
-//         res.status(200).json(newMessage);
-//     } catch (error) {
-//         console.log("Error in sendMessages controller: ", error.message);
-//         res.status(500).json({ message: "Internal Server Error" });
-//     }
-// }
+        res.status(200).json(newMessage);
+    } catch (error) {
+        console.log("Error in sendMessages controller: ", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
